3347e3277aa7d5b13c8a2aaf7a68070b9350a14a
PRAVIN RATHOD <133858500+pravinxrathod@users.noreply.github.com>
2023-06-23 11:48:52 +0530

    Delete portfolio directory

--- portfolio/scripts/responsive.js
+++
@@ -1,9 +0,0 @@
-function toggleNavbar(event){
-    let navlinks = document.getElementById("navlinks");
-    if(navlinks.className.includes("responsive")){
-        navlinks.classList.remove("responsive");
-    }
-    else{
-        navlinks.classList.add("responsive");
-    }
-}
